75022bbfd0f243340abf41b6daef10d216043a54
Erik G <er5402gi-s@student.lu.se>
2021-04-26 15:17:56 +0200

    removed imports

--- core/src/se/lth/MAMN01/team4/boatgame/MainMenuScreen.java
+++ core/src/se/lth/MAMN01/team4/boatgame/MainMenuScreen.java
@@ -3,16 +3,9 @@ package se.lth.MAMN01.team4.boatgame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
