0ec959abcedb525c59ce23080babda820e9c7e99
juliusMalmstrom <mrmalmstrom1@gmail.com>
2021-05-04 10:24:44 +0200

    Cloud-klass utan något i

---
+++ core/src/se/lth/MAMN01/team4/boatgame/Cloud.java
@@ -0,0 +1,17 @@
+package se.lth.MAMN01.team4.boatgame;
+
+public class Cloud implements GameObject{
+    public Cloud(){
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
